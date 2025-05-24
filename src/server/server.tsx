9ea4ef7500e31e13d915a0b53b5b9a9e45c27ea1
Martin Elsaesser <marels@aon.at>
2025-05-24 22:06:48 +0200

    add beercss lib

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -24,6 +24,18 @@ app.get(
 			return (
 				<html>
 					<head>
+						<link
+							href="https://cdn.jsdelivr.net/npm/beercss@3.11.10/dist/cdn/beer.min.css"
+							rel="stylesheet"
+						/>
+						<script
+							type="module"
+							src="https://cdn.jsdelivr.net/npm/beercss@3.11.10/dist/cdn/beer.min.js"
+						></script>
+						<script
+							type="module"
+							src="https://cdn.jsdelivr.net/npm/material-dynamic-colors@1.1.2/dist/cdn/material-dynamic-colors.min.js"
+						></script>
 						<link rel="stylesheet" href="/static/css/index.css" />
 						<link rel="stylesheet" href="/static/build/client.css" />
 					</head>
