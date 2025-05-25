f4a362bb1d76b96fae38ee96bc1b3ba77876e6e5
Martin Elsaesser <marels@aon.at>
2025-05-25 12:57:15 +0200

    rework css loader hook

--- esm-hooks/css-hook.mjs
+++ esm-hooks/css-hook.mjs
@@ -1,4 +1,4 @@
-console.log("css-loader.js");
+// source: https://nodejs.org/api/module.html#hooks
 export async function load(url, context, nextLoad) {
 	if (url.endsWith(".css")) {
 		return { source: "", format: "module", shortCircuit: true };
