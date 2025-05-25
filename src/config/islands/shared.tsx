30782b52930dfd1a42cfe4b586b1eca667cdc1a5
Martin Elsaesser <marels@aon.at>
2025-05-25 16:50:02 +0200

    add animations to expansion component

--- src/config/islands/shared.tsx
+++ src/config/islands/shared.tsx
@@ -1,5 +1,6 @@
 import App from "../../frontend/components/App.js";
 import Counter from "../../frontend/components/Counter.js";
+import { Expansion } from "../../frontend/components/Expansion.js";
 
 // register islands
-export const registeredIslands = [Counter, App];
+export const registeredIslands = [Counter, App, Expansion];
